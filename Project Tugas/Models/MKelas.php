c141277d32990b17dac6bcaaeb0f21a7b6f958a6
nenielsasari <141195313+nenielsasari@users.noreply.github.com>
2023-08-23 12:54:03 +0700

    Add files via upload

---
+++ Project Tugas/Models/MKelas.php
@@ -0,0 +1,62 @@
+<?php
+    require('../Config/Database.php');
+
+    Class MKelas{
+
+
+        function Simpan($nama){
+            global $koneksi; // Access the global $koneksi variable
+            $sql="insert into kelas values('".$nama."')";
+            $res = mysqli_query($koneksi,$sql);
+            // return
+            if($res == 1) {
+                // return "Data [".$nis." - ".$nama. "] Berhasil Disimpan";
+                return "Berhasil";
+            }
+
+        }
+
+        // function Update($nama, $newNama) {
+        //     global $koneksi; // Access the global $koneksi variable
+            
+        //     $sql = "UPDATE kelas SET nama = '$newNama' WHERE nama = '$nama'";
+        //     $res = mysqli_query($koneksi, $sql);
+        //     if ($res == 1) {
+        //         return "Berhasil";
+        //     } else {
+        //         return "Gagal";
+        //     }
+        // }
+
+        function Update($nama, $newName) {
+            global $koneksi; // Access the global $koneksi variable
+            
+            $sql = "UPDATE kelas SET nama = '$newName' WHERE nama = '$nama'";
+
+            try {
+                $res = mysqli_query($koneksi, $sql);
+                return "Berhasil";
+            } catch (mysqli_sql_exception $e) {
+                var_dump($e->getMessage());
+                return "Gagal: " . $e->getMessage();
+            }
+            
+        }
+        
+
+        function Delete($nama) {
+            global $koneksi; // Access the global $koneksi variable
+            
+            $sql = "DELETE FROM kelas WHERE nama = '$nama'";
+            $res = mysqli_query($koneksi, $sql);
+        
+            if ($res == 1) {
+                return "Berhasil";
+            } else {
+                return "Gagal";
+            }
+        }
+        
+        
+    }
+?>
